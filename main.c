bb5eda07976e7a0d9e5a0c25f90a0f8fe2783dc0
Agathe Ligouy <aligouy@c4b7c6.42berlin.de>
2026-06-05 17:22:49 +0200

    added comments to main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aligouy <aligouy@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 10:43:27 by aligouy           #+#    #+#             */
-/*   Updated: 2026/06/05 17:18:26 by aligouy          ###   ########.fr       */
+/*   Updated: 2026/06/05 17:22:32 by aligouy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ int	main(int argc, char **argv)
 
 	a = NULL;
 	// this function turns the input into an arr and handles the string format 'x y z'
-	// potentially we don't need to handle this edge case
+	// potentially we don't need to handle the string edge case
 	arr = process_input(argc, argv);
 	if (!arr)
 		return (1);
-	// now we process the arr
+	// now we process the arr and create stack a
 	asize = fill_stack(&a, arr);
-	//print stack a just for fun
+	//print stack a just for fun - remove for final submission
 	if (asize > 1)
 		print_stack(a, asize);
 	// compute disorder value
